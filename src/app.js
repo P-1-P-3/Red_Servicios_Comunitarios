02e263035f8e741163dc182f3df53ab797ef92ad
William <williampimo@gmail.com>
2026-05-03 22:33:36 -0500

    feat: funcionalidad basica del carrito completada por William

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { userRoutes } from './routes/user.routes.js';
 import { resError } from './middleware/resError.js';
 import 'dotenv/config';
+import { cartRoutes } from './routes/cart.routes.js';
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 
 // Definir las rutas aquí, por ejemplo:
 app.use('/users', userRoutes());
+app.use('/cart', cartRoutes());
 
 // ------------------------------------
 // Middleware para manejar errores
