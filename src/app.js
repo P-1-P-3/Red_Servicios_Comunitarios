07f7d42b16d93e105a26716b2cf099dc97e32c31
P1P3 <p1p3.11.13@gmail.com>
2026-05-03 22:26:33 -0500

    feat: add CORS support to the application

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { userRoutes } from './routes/user.routes.js';
 import { servicesRoutes } from './routes/services.routes.js';
 import { resError } from './middleware/resError.js';
@@ -7,6 +8,7 @@ import 'dotenv/config';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // Definir las rutas aquí, por ejemplo:
 app.use('/users', userRoutes());
