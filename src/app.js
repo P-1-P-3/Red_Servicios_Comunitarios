a0ec1f7696f42059e7f83b40520098cefb3dba66
Alexandra-Nic <npardoto@estudiante.ibero.edu.co>
2026-05-03 20:33:43 -0500

    add cambios nicoll pardo

--- src/app.js
+++ src/app.js
@@ -1,20 +1,26 @@
 import express from 'express';
+import cors from 'cors';
 import { userRoutes } from './routes/user.routes.js';
 import { resError } from './middleware/resError.js';
 import 'dotenv/config';
 
 const app = express();
 
+// 🔥 AGREGA ESTO (ANTES de las rutas)
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
+
 app.use(express.json());
 
-// Definir las rutas aquí, por ejemplo:
+// Rutas
 app.use('/users', userRoutes());
 
-// ------------------------------------
-// Middleware para manejar errores
+// Middleware errores
 app.use(resError);
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
